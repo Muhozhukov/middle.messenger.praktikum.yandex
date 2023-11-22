3765322771da6d0439e9d24dbe8cf5f1193469cf
Никита <pravoepredserdie@gmail.com>
2023-11-22 21:27:40 +0300

    fix links, fonts

--- src/client/pages/profile/profile.js
+++ src/client/pages/profile/profile.js
@@ -2,21 +2,21 @@ import handlebars from 'handlebars';
 import profile from './profile.tmpl';
 
 const template = handlebars.compile(profile);
-const buttons = {
-  buttons: [
-    {
-      buttonText: 'Изменить данные',
-      buttonClass: 'button_text button_profile',
-    },
-    {
-      buttonText: 'Изменить пароль',
-      buttonClass: 'button_text button_profile',
-    },
-    {
-      buttonText: 'Выйти',
-      buttonClass: 'button_text button_profile button_danger',
-    },
-  ],
-};
-
-document.getElementById('profile').innerHTML = template(buttons);
+const buttons = [
+  {
+    buttonText: 'Изменить данные',
+    buttonClass: 'button_text button_profile',
+  },
+  {
+    buttonText: 'Изменить пароль',
+    buttonClass: 'button_text button_profile',
+  },
+  {
+    buttonText: 'Выйти',
+    buttonClass: 'button_text button_profile button_danger',
+  },
+];
+document.getElementById('profile').innerHTML = template({
+  buttons,
+  profileName: 'Имя для профиля',
+});
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import path, { resolve, dirname } from 'path';
+import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 dotenv.config();
@@ -9,27 +9,8 @@ const __dirname = dirname(__filename);
 const distPath = resolve(__dirname, '../../dist');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.use(express.static(resolve(distPath)));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(distPath, 'index.html'));
-});
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(distPath, '/pages/login/login.html'));
-});
-app.get('/signup', (req, res) => {
-  res.sendFile(path.join(distPath, '/pages/signup/signup.html'));
-});
-app.get('/chat', (req, res) => {
-  res.sendFile(path.join(distPath, '/pages/chat/chat.html'));
-});
-app.get('/error', (req, res) => {
-  res.sendFile(path.join(distPath, '/pages/errorPage/errorPage.html'));
-});
-app.get('/profile', (req, res) => {
-  res.sendFile(path.join(distPath, '/pages/profile/profile.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
