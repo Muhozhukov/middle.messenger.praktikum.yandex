3d9b6bc812d1fc48fd339ef7e4ec81c2b6b6168d
Nikita <65251313+Muhozhukov@users.noreply.github.com>
2023-11-22 22:08:58 +0300

    Delete dist directory

--- dist/assets/errorPage-203aa059.js
+++
@@ -1,8 +0,0 @@
-import"./modulepreload-polyfill-3cfb730f.js";import{h as t,b as e}from"./button.tmpl-a4767006.js";t.registerPartial("buttonPartial",e);const r=`
-<main class="error-page">
-  <h1 class="page-title">{{errorCode}}</h1>
-  <p class="page-description">{{description}}</p>
-  <a href="/">
-    {{>buttonPartial buttonText="Назад к чатам" buttonClass="button_text"}}
-  </a>
-</main>`,a=t.compile(r);document.getElementById("errorPage").innerHTML=a({errorCode:404,description:"Не туда попали"});
--- dist/assets/sendMessageIcon-9948390f.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("circleIcon-56da6ae1.svg",import.meta.url).href,a=""+new URL("sendMessageIcon-053ef239.svg",import.meta.url).href;export{e as a,a as b};
